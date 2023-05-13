513e26b66c77081df641be78cce4e52156c4c5be
Majid Igale <majidigale18@gmail.com>
2023-05-13 16:08:00 -0500

    broken apart problem, got my post function to store the data first

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,8 +3,32 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jquery is good!')
+    $('#all-control').on('submit', postCalc)
     
 }
+let operator;
+//created a global operator for to set in my functions to the calculation inputs.
+function postCalc(event){
+event.preventDefault();
+
+let firstInput = $('#first-number').val()
+let secondInput = $('#second-number').val()
+let operator = $('#all-ops').val()
+
+//created my inputs to be able to show up on my server and save the data that is collected.
+
+$.ajax({
+    method: 'POST',
+    url: '/history',
+    data: {
+        firstInput,
+        secondInput,
+        operator
+    }
+})
+
+}
+
 
 
 // function postInput(){
--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const app = express();
 
 const port = 5000;
 
-const history = []
+const history = [
+    
+]
 
 app.use(express.static('server/public'))
 
@@ -15,11 +17,22 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 
 
-app.get('/history', function (req,res){
-    console.log('request for the guess was made');
-    res.send()
-})
+// app.get('/history', function (req,res){
+//     console.log('request for the guess was made',history);
+//     res.send(history)
 
+// })
+
+// app.post('calculation', function (req,res){
+   
+// })
+
+app.post('/history', function (req , res){
+ 
+    history.push(req.body)
+    console.log('got the history', history)
+    res.send(201)
+})
 
 
 
